2bdf84a3aa8ad7bea34ea6d9bad6d8b6d1c78bdc
ZhaoYuyuan <wp9651610794@live.com>
2023-05-25 04:20:46 +0800

    1

---
+++ mover.js
@@ -0,0 +1,58 @@
+// Acceleration Vector
+// The Nature of Code
+// The Coding Train / Daniel Shiffman
+// https://youtu.be/T84AWnntxZA
+// https://thecodingtrain.com/learning/nature-of-code/1.6-acceleration-vector.html
+// https://editor.p5js.org/codingtrain/sketches/OjCfrdWX
+
+class Mover {
+    constructor(x, y) {
+        this.pos = createVector(x, y);
+        // this.vel = createVector(1, -1);
+        this.vel = p5.Vector.random2D();
+        this.vel.mult(random(3));
+        this.mouse = createVector(random(width), random(height));
+        let i = floor(random(0,3));
+        if (i == 0){
+            this.r = 0;
+            this.g = random(255);
+            this.b = random(255);
+        } 
+        if (i == 1){
+            this.r = random(255);
+            this.g = 0;
+            this.b = random(255);
+        }
+        if (i == 2){
+            this.r = random(255);
+            this.g = random(255);
+            this.b = 0;
+        }
+        console.log(i);
+ 
+    }
+
+    updateP() {
+        this.mouse = createVector(random(width), random(height));
+        //mouse = p5.Vector.random2D();
+    }
+
+    update() {
+        //var mouse = createVector(random(width), random(height));
+        this.acc = p5.Vector.sub(this.mouse, this.pos);
+        this.acc.setMag(1);
+
+        this.vel.add(this.acc);
+        this.vel.limit(2);
+
+        this.pos.add(this.vel);
+    }
+
+    show() {
+
+        paintLayer.noStroke();
+        //paintLayer.fill(random(255),random(255),random(255));
+        paintLayer.fill(this.r,this.g,this.b);
+        paintLayer.ellipse(this.pos.x, this.pos.y, 20);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 var video;
-var gr = 128;
-var gg = 128;
-var gb = 128;
 var disGlobal = 0;
+var rectNum = 0;
+var rectNumDelta = 1;
+var video;
+var videoLayer;
+var paintLayer;
+var mover;
+var moverDis;
+var dotsAll = [];
 
 var options = {
   video: {
     facingMode: {
-      exact: "user"
+      exact: "environment"
+      //exact: "user"
     }
   }
 };
@@ -15,47 +21,105 @@ var options = {
 function setup() {
   background(160);
   video = createCapture(options);
+  videoLayer = createCanvas(400, 400);
+  paintLayer = createGraphics(400, 400);
   pixelDensity(1);
   textSize(20);
   textAlign(CENTER, CENTER);
+  video.hide();
 }
 
 function draw() {
-  createCanvas(video.width, video.height);
-  background(160);
-  loadPixels();
+  if (frameCount == 1) {
+
+    mover = new Mover(width, height);
+  }
+
+  if (frameCount == 120) {
+    resizeCanvas(video.width, video.height);
+    paintLayer.resizeCanvas(video.width, video.height);
+    for (var i = 1; i <= 5; i++) {
+      mover = new Mover(random(video.width), random(video.height));
+      dotsAll.push(mover);
+    }
+
+  }
+
   video.loadPixels();
+  detectColor();
+  textAlign(CENTER);
+  disGlobal = floor(disGlobal / 9000 * 255);
+  rectNum = floor(abs((disGlobal - 128) / 128 * 20));
 
-  for (var y = 0; y < height; y++) {
-    for (var x = 0; x < width; x++) {
-      var index = (x + y * width) * 4;
-      var r = video.pixels[index + 0];
-      var g = video.pixels[index + 1];
-      var b = video.pixels[index + 2];
-
-      pixels[index + 0] = r;
-      pixels[index + 1] = g;
-      pixels[index + 2] = b;
-      pixels[index + 3] = 255;
+  if (disGlobal > 150 && dotsAll.length < 500) {
+    mover = new Mover(random(video.width), random(video.height));
+    dotsAll.push(mover);
+  } else if (disGlobal < 100 && dotsAll.length > 0) {
+    dotsAll.pop();
+  }
+  /*
+  if (frameCount % (21 - rectNum) == 0) {
+    if (disGlobal > 128) {
+      paintLayer.noStroke();
+      paintLayer.fill(random(255), random(255), random(255));
+      paintLayer.push();
+      paintLayer.translate(random(width), random(height));
+      paintLayer.rotate(random(360));
+      paintLayer.rect(0, 0, random(30), random(30));
+      paintLayer.pop();
+    } else {
+      paintLayer.erase();
+      paintLayer.rect(random(width), random(height), 200);
+      paintLayer.noErase();
     }
   }
-  detectColor();
-  updatePixels();
-  textAlign(CENTER);
+  */
+
+
+  if (frameCount % 45 == 0) {
+    for (var i = 0; i < dotsAll.length; i++) {
+      moverDis = dotsAll[i];
+      moverDis.updateP();
+    }
+
+  }
+  for (var i = 0; i < dotsAll.length; i++) {
+    moverDis = dotsAll[i];
+    moverDis.update();
+  }
+  // mover.update();
+  var pic = video.get();
+  paintLayer.tint(255, 20);
+  paintLayer.image(pic, 0, 0);
+  for (var i = 0; i < dotsAll.length; i++) {
+    moverDis = dotsAll[i];
+    moverDis.show();
+  }
+  // mover.show();
   drawWords(width * 0.95, height * 0.95);
+  disGlobal = 0;
+
+  image(video, 0, 0);
+
+  image(paintLayer, 0, 0);
+
+
+
 }
 
 function drawWords(x, y) {
-  fill(255);
-  text(disGlobal, x, y);
-  disGlobal = 0;
+  paintLayer.fill(0);
+  paintLayer.rect(x - 20, y - 20, 100, 40);
+  paintLayer.fill(255);
+
+  paintLayer.text(disGlobal, x, y);
+
 }
 
 function detectColor() {
   var dr, dg, db, dis, dx, dy, index, indexLast;
   for (var i = 1; i <= 10; i++) {
     for (var j = 1; j <= 10; j++) {
-
       dx = floor(width / 10 * j) - 1;
       dy = floor(height / 10 * i) - 1;
       index = (dx + dy * width) * 4;
@@ -72,12 +136,14 @@ function detectColor() {
         dr = video.pixels[index + 0] - video.pixels[indexLast + 0];
         dg = video.pixels[index + 1] - video.pixels[indexLast + 1];
         db = video.pixels[index + 2] - video.pixels[indexLast + 2];
-        console.log(dr);
+        //console.log(dr);
       }
       dis = Math.sqrt(Math.pow(dr, 2) + Math.pow(dg, 2) + Math.pow(db, 2));
       dis = floor(dis);
+      //console.log(dis);
       disGlobal += dis;
       indexLast = index;
+
     }
   }
 }
